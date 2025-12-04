474250c1b992327ff730dc39d1e8c48e19f6a664
Xaviersnm <1670100@uab.cat>
2025-12-04 16:56:49 +0100

    update

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ if __name__ == '__main__':
         transforms.ToTensor()
     ])
     
-    ds = PokemonMetaDataset('pokemon_data_gen1-5.csv', 'pokemon_sprites', transform=transform_pipeline)
+    ds = PokemonMetaDataset('pokemon_data_linked.csv', 'pokemon_sprites', transform=transform_pipeline)
 
     # 2. Create Loaders
     # Train on Gen 1, 2, 3. Test on Gen 4.
     train_labels, test_labels, val_labels = get_structured_splits(
         ds, 
         split_mode='generation', 
-        train_vals=['generation-i', 'generation-ii'],
-        val_vals=['generation-iii'],
+        train_vals=['generation-i', 'generation-ii', 'generation-iii'],
+        #val_vals=['generation-iii'],
         test_vals=['generation-iv']
     )
 
@@ -38,7 +38,26 @@ if __name__ == '__main__':
     # test_vals=['Dragon', 'Ghost', 'Ice', 'Steel']
     # )
 
-    train_loader, test_loader, val_loader = get_meta_dataloaders(ds, train_labels, test_labels, val_labels, N_WAY, N_SHOT, N_QUERY, EPISODES_PER_EPOCH)
+    train_loader, test_loader, val_loader = get_meta_dataloaders_pokedex(ds, train_labels, test_labels, val_labels, N_WAY, N_SHOT, N_QUERY, EPISODES_PER_EPOCH)
+    
+    oak_train_loader, oak_test_loader, oak_val_loader = get_meta_dataloaders_oak(ds, train_labels, test_labels, val_labels, N_WAY, N_SHOT, N_QUERY, EPISODES_PER_EPOCH)
+
+    # --- VISUALIZATION BLOCK ---
+    print("Fetching Pokedex Episode (Standard)...")
+    pok_images, pok_labels = next(iter(train_loader))
+    visualize_episode(pok_images, n_way=N_WAY, n_shot=N_SHOT, n_query=N_QUERY)
+    pok_images, pok_labels = next(iter(val_loader))
+    visualize_episode(pok_images, n_way=N_WAY, n_shot=N_SHOT, n_query=N_QUERY)
+    pok_images, pok_labels = next(iter(test_loader))
+    visualize_episode(pok_images, n_way=N_WAY, n_shot=N_SHOT, n_query=N_QUERY)
+
+    print("Fetching Oak Episode (Evolutionary)...")
+    oak_images, oak_labels = next(iter(oak_train_loader))
+    visualize_episode(oak_images, n_way=N_WAY, n_shot=N_SHOT, n_query=N_QUERY)      
+    oak_images, oak_labels = next(iter(oak_val_loader))
+    visualize_episode(oak_images, n_way=N_WAY, n_shot=N_SHOT, n_query=N_QUERY)
+    oak_images, oak_labels = next(iter(oak_test_loader))
+    visualize_episode(oak_images, n_way=N_WAY, n_shot=N_SHOT, n_query=N_QUERY)
 
     # 3. INITIALIZE MODEL
     print("Initializing Model...")
